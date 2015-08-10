1c952160ecd95f30fcc0617b74aef548aea827dd
Igor Mucsicska <mucsi_96@yahoo.com>
2015-08-10 14:16:08 +0200

    No configuration warning added

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var glob = require('glob');
 var path = require('path');
 
 module.exports = function (dirname) {
-    var res = {};
+    var res = {},
+        confFound = false;
+
     glob.sync(path.join(dirname, '*.js'))
         .map(function (fname) { return require(path.join(process.cwd(), fname)); })
-        .forEach(function (obj) { merge.recursive(res, obj); });
+        .forEach(function (obj) {
+            merge.recursive(res, obj);
+            confFound = true
+        });
+
+    if (!confFound) {
+        console.log('Waring: no configuration was found in ' + path.normalize(dirname));
+    }
 
     return res;
 };
