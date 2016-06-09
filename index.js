e85a53a44fd8d1a20540e9f519e1e567eacda15b
Péter Kovács <kiwep@ewn.gs>
2016-06-09 13:57:19 +0200

    Minor code fixes

--- index.js
+++ index.js
@@ -3,20 +3,25 @@
 var merge = require('merge');
 var glob = require('glob');
 var path = require('path');
+var assert = require('assert');
 
 module.exports = function (dirname) {
     var res = {},
         confFound = false;
 
+    assert(typeof dirname === 'string', 'dirname parameter is required!');
+
     glob.sync(path.join(dirname, '*.js'))
-        .map(function (fname) { return require(path.join(process.cwd(), fname)); })
+        .map(function (fname) {
+            return require(path.join(process.cwd(), fname));
+        })
         .forEach(function (obj) {
             merge.recursive(res, obj);
             confFound = true;
         });
 
     if (!confFound) {
-        console.log('Waring: no configuration was found in ' + path.normalize(dirname));
+        console.log('Warning: no configuration found in ' + path.normalize(dirname));
     }
 
     return res;
